b53cffdbf002f2870c40a9264812413f1f80b919
fvazquezf <fvazquezf@fi.uba.ar>
2022-04-07 16:30:12 -0300

    primer version del log in

---
+++ app/src/main/java/com/example/spotifiubyfy01/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.spotifiubyfy01
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "com.example.spotifiubyfy01.MESSAGE"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View) {
+        var editText = findViewById<EditText>(R.id.registration_username)
+        val username = editText.text.toString()
+        editText = findViewById<EditText>(R.id.registration_password)
+        val password = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra("new_username", username)
+            putExtra("new_password", password)
+
+        }
+        startActivity(intent)
+
+    }
+}
