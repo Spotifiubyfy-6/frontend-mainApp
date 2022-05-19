4908050e3170485d96154fcc523b03626bd45f87
andrezszambrano <andreskilla2000@gmail.com>
2022-05-19 12:44:24 -0300

    MainPage activity is added. It is viewed after the user logs in. It has a Profile and a Search button

---
+++ app/src/main/java/com/example/spotifiubyfy01/MainPage.kt
@@ -0,0 +1,21 @@
+package com.example.spotifiubyfy01
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainPage: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_page)
+
+        val profile = findViewById<Button>(R.id.profile_button)
+        profile.setOnClickListener {
+            val intent = Intent(this, ProfilePage::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+}
