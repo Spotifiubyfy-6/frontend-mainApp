33198774f36a16769c1e4a28b9c5ccb78d47ef12
andrezszambrano <andreskilla2000@gmail.com>
2022-05-20 14:25:17 -0300

    More artists are added to the list

---
+++ app/src/main/java/com/example/spotifiubyfy01/search/DataSource.kt
@@ -0,0 +1,26 @@
+package com.example.spotifiubyfy01.search
+
+class DataSource{
+    companion object {
+        fun createDataSet(): ArrayList<Artist> {
+            val list = ArrayList<Artist>()
+            list.add(Artist("The Beatles", image_link))
+            list.add(Artist("The Rolling Stones", image_link))
+            list.add(Artist("Metallica", image_link))
+            list.add(Artist("Green Day", image_link))
+            list.add(Artist("The Smiths", image_link))
+            list.add(Artist("Dua Lipa", image_link))
+            list.add(Artist("Canserbero", image_link))
+            list.add(Artist("TuPac", image_link))
+            list.add(Artist("Eminem", image_link))
+            list.add(Artist("Muse", image_link))
+            list.add(Artist("Coldplay", image_link))
+            list.add(Artist("1975", image_link))
+            list.add(Artist("Led Zeppelin", image_link))
+            list.add(Artist("Ariana Grande", image_link))
+            list.add(Artist("Juan Luis Guerra", image_link))
+            list.add(Artist("Biggie", image_link))
+            return list
+        }
+    }
+}
---
+++ app/src/main/java/com/example/spotifiubyfy01/search/SearchPage.kt
@@ -0,0 +1,49 @@
+package com.example.spotifiubyfy01.search
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.spotifiubyfy01.R
+
+class SearchPage : AppCompatActivity() {
+
+    private lateinit var artistAdapter : ArtistRecyclerAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_page)
+        val search = findViewById<EditText>(R.id.search_textfield)
+        search.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                Log.d("TAG", search.text.toString())
+            }
+        })
+        initRecyclerView()
+        addDataSet()
+    }
+
+    private fun addDataSet() {
+        val data = DataSource.createDataSet()
+        artistAdapter.submitList(data)
+    }
+
+    private fun initRecyclerView() {
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(this@SearchPage)
+            artistAdapter = ArtistRecyclerAdapter()
+            adapter = artistAdapter
+        }
+    }
+}
