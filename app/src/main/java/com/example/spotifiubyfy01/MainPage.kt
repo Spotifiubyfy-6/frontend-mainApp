d5112874f691ae2e0c507847fc842305467bc08d
andrezszambrano <andreskilla2000@gmail.com>
2022-05-20 12:24:49 -0300

    RecyclerView is added to search activity

--- app/src/main/java/com/example/spotifiubyfy01/MainPage.kt
+++ app/src/main/java/com/example/spotifiubyfy01/MainPage.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.spotifiubyfy01.search.SearchPage
 
 class MainPage: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
