aa9f4ccfddbfe0ff1dc4fcb77ce24285ceb933e8
andrezszambrano <andreskilla2000@gmail.com>
2022-05-25 20:13:19 -0300

    ArtistPage shows albums of the artist

--- app/src/main/java/com/example/spotifiubyfy01/ArtistProfile/ArtistPage.kt
+++ app/src/main/java/com/example/spotifiubyfy01/ArtistProfile/ArtistPage.kt
@@ -1,12 +1,16 @@
-package com.example.spotifiubyfy01.search.adapter
+package com.example.spotifiubyfy01.ArtistProfile
 
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.spotifiubyfy01.ArtistProfile.adapter.AlbumRecyclerAdapter
 import com.example.spotifiubyfy01.R
 import com.example.spotifiubyfy01.search.Artist
+import com.example.spotifiubyfy01.search.adapter.ArtistRecyclerAdapter
 
 class ArtistPage: AppCompatActivity() {
 
@@ -19,7 +23,13 @@ class ArtistPage: AppCompatActivity() {
         artistName.text = artist.username
         Glide.with(image.context).load(artist.image).into(image)
 
-
-
+        initRecyclerView()
+    }
+    private fun initRecyclerView() {
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,
+                                    false)
+        recyclerView.adapter =
+            AlbumRecyclerAdapter(AlbumDataSource.createAlbumList())
     }
 }
