8fe43e0e60fcb67cc1bcd81e8e1cb91ed7f1eae2
andrezszambrano <andreskilla2000@gmail.com>
2022-05-26 14:42:52 -0300

    ArtistViewHolder extends abstract class SearchViewHolder

--- app/src/main/java/com/example/spotifiubyfy01/search/adapter/ArtistViewHolder.kt
+++ app/src/main/java/com/example/spotifiubyfy01/search/adapter/ArtistViewHolder.kt
@@ -7,13 +7,15 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.spotifiubyfy01.R
 import com.example.spotifiubyfy01.search.Artist
+import com.example.spotifiubyfy01.search.SearchItem
 
-class ArtistViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+class ArtistViewHolder(view: View) : SearchViewHolder(view) {
 
     private val artistName: TextView = view.findViewById(R.id.artist_name)
     private val image: ImageView = view.findViewById(R.id.artist_image)
 
-    fun render(artist: Artist, onClickListener: (Artist) -> Unit) {
+    override fun render(item: SearchItem, onClickListener: (SearchItem) -> Unit) {
+        val artist = item as Artist
         artistName.text = artist.username
         Glide.with(image.context).load(artist.image).into(image)
         itemView.setOnClickListener { onClickListener(artist) }
