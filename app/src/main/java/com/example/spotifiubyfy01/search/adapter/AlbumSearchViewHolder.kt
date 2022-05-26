8fc1831ff0520ffe9fc1ff9b0c034b1d409aec11
andrezszambrano <andreskilla2000@gmail.com>
2022-05-26 15:34:54 -0300

    AlbumSearchViewHolder is created along with its layout.xml

---
+++ app/src/main/java/com/example/spotifiubyfy01/search/adapter/AlbumSearchViewHolder.kt
@@ -0,0 +1,26 @@
+package com.example.spotifiubyfy01.search.adapter
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.example.spotifiubyfy01.R
+import com.example.spotifiubyfy01.artistProfile.Album
+import com.example.spotifiubyfy01.search.Artist
+import com.example.spotifiubyfy01.search.SearchItem
+
+class AlbumSearchViewHolder(view: View) : SearchViewHolder(view) {
+
+    private val albumName: TextView = view.findViewById(R.id.album_name)
+    private val artistName: TextView = view.findViewById(R.id.artist_name)
+    private val albumImage: ImageView = view.findViewById(R.id.album_image)
+
+    override fun render(item: SearchItem, onClickListener: (SearchItem) -> Unit) {
+        val album = item as Album
+        albumName.text = album.album_name
+        val text = "Album by" + album.artist_name
+        artistName.text = text
+        Glide.with(albumImage.context).load(album.album_image).into(albumImage)
+        itemView.setOnClickListener { onClickListener(album) }
+    }
+}
