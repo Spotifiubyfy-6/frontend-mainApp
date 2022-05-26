24fd29c49fb0866b8b67bbb3b1135e4f53c0d512
andrezszambrano <andreskilla2000@gmail.com>
2022-05-26 14:41:34 -0300

    SearchViewHolder abstract is implemented. Also a method that creates and returns a SearchViewHolder according to the items type is implemented

---
+++ app/src/main/java/com/example/spotifiubyfy01/search/adapter/SearchViewHolder.kt
@@ -0,0 +1,29 @@
+package com.example.spotifiubyfy01.search.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.spotifiubyfy01.R
+import com.example.spotifiubyfy01.search.SearchItem
+
+fun createRespectiveHolder(parent: ViewGroup, viewType: Int) : SearchViewHolder{
+    val holder =
+        when (viewType) {
+            0 ->    {
+                val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.layout_artist_list_item, parent, false)
+                ArtistViewHolder(view)
+            }
+            else -> {
+                val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.layout_artist_list_item, parent, false)
+                ArtistViewHolder(view)
+            }
+        }
+    return holder
+}
+
+abstract class SearchViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    abstract fun render(item: SearchItem, onClickListener: (SearchItem) -> Unit)
+}
