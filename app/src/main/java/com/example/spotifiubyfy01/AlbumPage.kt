71285511b70fd2bf054083ad3f8d470c7c8f9d2d
andrezszambrano <andreskilla2000@gmail.com>
2022-05-26 02:43:50 -0300

    AlbumPage uses SongRecyclerAdapter

--- app/src/main/java/com/example/spotifiubyfy01/AlbumPage.kt
+++ app/src/main/java/com/example/spotifiubyfy01/AlbumPage.kt
@@ -1,10 +1,21 @@
 package com.example.spotifiubyfy01
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.spotifiubyfy01.artistProfile.Album
+import com.example.spotifiubyfy01.artistProfile.Song
+import com.example.spotifiubyfy01.artistProfile.adapter.AlbumRecyclerAdapter
+import com.example.spotifiubyfy01.artistProfile.adapter.SongRecyclerAdapter
+import com.example.spotifiubyfy01.search.VolleyCallBack
 
 class AlbumPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,52 +23,23 @@ class AlbumPage : AppCompatActivity() {
         setContentView(R.layout.activity_album_page)
         val album = intent.extras?.get("Album") as Album
         findViewById<TextView>(R.id.albumName).text = album.album_name
-        /*val songs = JSONArray()
+        findViewById<TextView>(R.id.artistName).text = album.artist_name
+        val image = findViewById<ImageView>(R.id.album_image)
+        Glide.with(image.context).load(album!!.album_image).into(image)
+        initRecyclerView(album.song_list)
+    }
 
-        val url = "http://spotifiubyfy-music.herokuapp.com/albums/"+"16"
-
-        val getRequest = StringRequest(
-            Request.Method.GET, url,
-            { response ->
-                val responseJson = JSONObject(response)
-                findViewById<TextView>(R.id.albumName).text = responseJson.getString("album_name")
-                songs.put(responseJson.getJSONArray("songs"))
-                findViewById<TextView>(R.id.songName).text = JSONObject(songs.getString(0).substring( 1, songs.getString(0).length - 1 )).getString("song_name")
-            },
-            { errorResponse -> val intent = Intent(this, PopUpWindow::class.java).apply {
-                val error = errorResponse.networkResponse.data.decodeToString().split('"')[3]
-                putExtra("popuptext", error)
+    private fun initRecyclerView(songList: List<Song>) {
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = SongRecyclerAdapter(songList) {song ->
+                onItemClicked(song)
             }
-                startActivity(intent)})
+    }
 
-        MyRequestQueue.getInstance(this).addToRequestQueue(getRequest)
-
-
-        val playSongButton = findViewById<Button>(R.id.playButton)
-        val app = (this.application as Spotifiubify)
-
-        playSongButton.setOnClickListener {
-            val storageName = "songs/"+JSONObject(songs.getString(0).substring( 1, songs.getString(0).length - 1 )).getString("storage_name")
-            val songRef =  app.getStorageReference().child(storageName)
-            songRef.downloadUrl.addOnSuccessListener { url ->
-                    val mediaPlayer = MediaPlayer().apply {
-                    setAudioAttributes(
-                        AudioAttributes.Builder()
-                            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-                            .setUsage(AudioAttributes.USAGE_MEDIA)
-                            .build()
-                    )
-                    setDataSource(url.toString())
-                    prepare() // might take long! (for buffering, etc)
-                    start()
-                }
-            }.addOnFailureListener {
-                // Handle any errors
-                Toast.makeText(app, "cant play song", Toast.LENGTH_LONG).show()
-            }
-        }
-
-*/
+    private fun onItemClicked(song: Song) {
+        //Do something with the Song
+        Log.d(TAG, song.song_name +" with id " + song.id.toString() + " made by " + song.artist)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -67,4 +49,5 @@ class AlbumPage : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
+
 }
