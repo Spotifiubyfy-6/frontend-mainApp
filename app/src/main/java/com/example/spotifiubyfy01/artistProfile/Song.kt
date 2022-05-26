06e2354169117642c7182e0a94761c18aa4962fe
andrezszambrano <andreskilla2000@gmail.com>
2022-05-26 01:31:25 -0300

    Song DataClass made Serializable so it can Album can be passed to AlbumActivityPage

--- app/src/main/java/com/example/spotifiubyfy01/artistProfile/Song.kt
+++ app/src/main/java/com/example/spotifiubyfy01/artistProfile/Song.kt
@@ -1,8 +1,10 @@
 package com.example.spotifiubyfy01.artistProfile
 
+import java.io.Serializable
+
 data class Song (
     var song_name: String,
     var album_id: Int,
     var id: Int,
     var storage_name: String
-)
+) : Serializable
