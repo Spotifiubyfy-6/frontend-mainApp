a8acd69722942755fa70e355fef17a62f2e54cd7
andrezszambrano <andreskilla2000@gmail.com>
2022-05-26 12:22:47 -0300

    Refactor: ArtistRecyclerView creates view holders according to the positions class

--- app/src/main/java/com/example/spotifiubyfy01/search/adapter/ArtistRecyclerAdapter.kt
+++ app/src/main/java/com/example/spotifiubyfy01/search/adapter/ArtistRecyclerAdapter.kt
@@ -6,14 +6,29 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.spotifiubyfy01.R
 import com.example.spotifiubyfy01.search.Artist
 
+
+
 class ArtistRecyclerAdapter(
     private var artistList: List<Artist>,
     private val onClickListener:(Artist) -> Unit
 ): RecyclerView.Adapter<ArtistViewHolder>() {
 
+    override fun getItemViewType(position: Int): Int {
+        val itemType = artistList[position].javaClass
+        if (itemType == Artist::javaClass)
+            return 0
+        else
+            return super.getItemViewType(position)
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArtistViewHolder {
+        val layout =
+            when (viewType) {
+                0 -> R.layout.layout_artist_list_item
+                else -> {1}
+            }
         val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.layout_artist_list_item, parent, false)
+            LayoutInflater.from(parent.context).inflate(layout, parent, false)
         return ArtistViewHolder(view)
     }
 
