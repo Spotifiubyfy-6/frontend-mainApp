de20e6707dd30842c73205b328bb0615762ec026
RVLareu <rlareu@fi.uba.ar>
2022-06-05 19:34:23 -0300

    + en el button de add to playlist

--- app/src/main/java/com/example/spotifiubyfy01/artistProfile/adapter/SongViewHolder.kt
+++ app/src/main/java/com/example/spotifiubyfy01/artistProfile/adapter/SongViewHolder.kt
@@ -1,6 +1,7 @@
 package com.example.spotifiubyfy01.artistProfile.adapter
 
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -12,10 +13,13 @@ class SongViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     private val songName: TextView = view.findViewById(R.id.song_name)
     private val artistName: TextView = view.findViewById(R.id.artist_name)
+    private val addToPlaylist : Button = view.findViewById(R.id.button)
 
     fun render(song: Song, onClickListener: (Song) -> Unit) {
         songName.text = song.song_name
         artistName.text = song.artist
+        addToPlaylist.text = "+"
+
         itemView.setOnClickListener { onClickListener(song) }
     }
 }
