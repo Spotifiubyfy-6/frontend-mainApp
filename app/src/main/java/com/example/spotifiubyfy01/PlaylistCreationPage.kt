7ceaf0f9ce31dadae5a6c8069ec2ce510bbf60b6
RVLareu <rlareu@fi.uba.ar>
2022-06-05 18:54:52 -0300

    modifica apariencia

---
+++ app/src/main/java/com/example/spotifiubyfy01/PlaylistCreationPage.kt
@@ -0,0 +1,11 @@
+package com.example.spotifiubyfy01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlaylistCreationPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_playlist_creation_page)
+    }
+}
