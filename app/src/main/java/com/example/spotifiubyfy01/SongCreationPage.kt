6e88a696fc0217861e84c21c183035853f3b2837
andrezszambrano <andreskilla2000@gmail.com>
2022-06-05 21:34:06 -0300

    When the upload button is clicked in SongCreationPage, the album id to do the POST is equivalent to the album chosen in the dropdown menu

--- app/src/main/java/com/example/spotifiubyfy01/SongCreationPage.kt
+++ app/src/main/java/com/example/spotifiubyfy01/SongCreationPage.kt
@@ -3,14 +3,12 @@ package com.example.spotifiubyfy01
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import android.widget.*
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.content.ContextCompat
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
@@ -18,22 +16,31 @@ import com.example.spotifiubyfy01.artistProfile.Album
 import org.json.JSONObject
 import java.io.File
 
-class SongCreationPage : AppCompatActivity() {
+class SongCreationPage : AppCompatActivity(), AdapterView.OnItemClickListener {
+
+    private var albumId: Int? = null
+    private var albumList: ArrayList<Album>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_song_creation_page)
         val dropDownMenu = findViewById<AutoCompleteTextView>(R.id.albums_names)
-        val albumList = intent.extras?.get("albums") as ArrayList<Album>?
+        albumList = intent.extras?.get("albums") as ArrayList<Album>?
         val albumNames = ArrayList<String>()
         if (albumList == null) {
             val albumName = intent.extras?.get("album_name") as String
             albumNames.add(albumName)
+            albumId = (intent.getStringExtra("album_id") as String).toInt()
         } else {
-            obtainAlbumNamesAndAddToList(albumList, albumNames)
+            obtainAlbumNamesAndAddToList(albumList!!, albumNames)
+            albumId = obtainAlbumIdOfPosition(albumList!!, 0)
         }
         dropDownMenu.setText(albumNames[0])
         val adapter = ArrayAdapter(this, R.layout.dropdown_item, albumNames)
-        dropDownMenu.setAdapter(adapter)
+        with(dropDownMenu) {
+            setAdapter(adapter)
+            onItemClickListener = this@SongCreationPage
+        }
 //      falta hacer esto mas explicativo, que se entienda mejor cuando se dan los permisos y eso
         val requestPermissionLauncher =
             registerForActivityResult(
@@ -73,7 +80,7 @@ class SongCreationPage : AppCompatActivity() {
 
             requestBody.put("song_name", songName.text.toString())
             requestBody.put("song_description", songDescription.text.toString())
-            requestBody.put("album_id", intent.getStringExtra("album_id"))
+            requestBody.put("album_id", albumId.toString())
 
             val url = "http://spotifiubyfy-music.herokuapp.com/music"
 
@@ -110,10 +117,29 @@ class SongCreationPage : AppCompatActivity() {
         }
     }
 
+    private fun obtainAlbumIdOfPosition(albumList: ArrayList<Album>, position: Int): Int {
+        return albumList[position].album_id.toInt()
+    }
+
     private fun obtainAlbumNamesAndAddToList(albumList: ArrayList<Album>,
                                              albumNames: ArrayList<String>) {
         for (album in albumList)
             albumNames.add(album.album_name)
+    }
+
+    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val albumName = parent?.getItemAtPosition(position).toString()
+        if (albumList == null)
+            return
+        albumId = obtainAlbumIdOfAlbumWithName(albumList!!, albumName)
+    }
+
+    private fun obtainAlbumIdOfAlbumWithName(albumList: ArrayList<Album>, albumName: String): Int {
+        for (album in albumList) {
+            if (album.album_name.equals(albumName))
+                return album.album_id.toInt()
+        }
+        return 0 //should not happen
     }
 }
 
