c0acd5caa997f7468aa4204beec75174b242eac5
andrezszambrano <andreskilla2000@gmail.com>
2022-06-10 11:44:45 -0300

    Date item is added to MessageItemEnum. Message now receives a string representing the hour

--- app/src/main/java/com/example/spotifiubyfy01/Messages/Message.kt
+++ app/src/main/java/com/example/spotifiubyfy01/Messages/Message.kt
@@ -7,12 +7,13 @@ fun convertIntToMessageEnum(value: Int): MessageEnum {
     return when(value) {
         0 -> MessageEnum.MESSAGE_RECEIVED
         1 -> MessageEnum.MESSAGE_SENT
+        2 -> MessageEnum.DATE
         else -> throw RuntimeException() //should never happen
     }
 }
 
 enum class MessageEnum {
-    MESSAGE_RECEIVED, MESSAGE_SENT; //NEVER CHANGE THIS ORDER
+    MESSAGE_RECEIVED, MESSAGE_SENT, DATE; //NEVER CHANGE THIS ORDER
 }
 
 abstract class MessageItem() {
@@ -24,7 +25,7 @@ data class Message(
     var receiver_id: Int,
     var sender_id: Int,
     var message: String,
-    var time: Time?
+    var time: String?
 ) : Serializable, MessageItem() {
     override fun getMessageType(): MessageEnum {
         if (requester_id == receiver_id)
