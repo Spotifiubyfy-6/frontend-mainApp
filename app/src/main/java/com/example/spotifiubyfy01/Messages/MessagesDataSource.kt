5c6bbd5f447f13eb6b6e58d1dd829fc45dfb22e5
andrezszambrano <andreskilla2000@gmail.com>
2022-06-10 15:25:14 -0300

    Logic set to get list of artists in getChatsOfArtistWithID method

--- app/src/main/java/com/example/spotifiubyfy01/Messages/MessagesDataSource.kt
+++ app/src/main/java/com/example/spotifiubyfy01/Messages/MessagesDataSource.kt
@@ -1,28 +1,100 @@
 package com.example.spotifiubyfy01.Messages
 
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresApi
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.example.spotifiubyfy01.MyRequestQueue
+import com.example.spotifiubyfy01.PopUpWindow
+import com.example.spotifiubyfy01.artistProfile.AlbumDataSource
 import com.example.spotifiubyfy01.search.Artist
 import com.example.spotifiubyfy01.search.VolleyCallBack
+import org.json.JSONArray
+import org.json.JSONObject
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 
 var image_link = "https://he.cecollaboratory.com/public/layouts/images/group-default-logo.png"
 
+class ChatList(
+    val numberOfArtist: Int,
+    val callBack: VolleyCallBack<ChatBundle>
+) {
+    var artistInserted = 0
+    val chatList = ArrayList<ChatBundle>(numberOfArtist)
+
+    fun addArtistWithIdToPositionInList(artist: Artist, position: Int, seen: Boolean) {
+        synchronized(this) {
+            chatList[position] = ChatBundle(artist, seen)
+            artistInserted++
+            if (artistInserted == numberOfArtist)
+                callBack.updateData(chatList)
+        }
+    }
+}
+
 class MessagesDataSource {
+
     companion object {
+
+        private fun searchRespectiveArtists(context: Context, idNSeenTuples: JSONArray,
+                                            callBack: VolleyCallBack<ChatBundle>) {
+            val numberOfArtists = idNSeenTuples.length()
+            val chatList = ChatList(numberOfArtists, callBack)
+            for (i in 0 until numberOfArtists) {
+                val idNSeenTuple = JSONObject(idNSeenTuples.get(i).toString())
+                this.searchArist(context, chatList, idNSeenTuple, i)
+            }
+        }
+
+        private fun searchArist(context: Context, chatList: ChatList, idNSeenTuple: JSONObject,
+                                position: Int) {
+            val artistId = idNSeenTuple.toString()
+            val url = "https://spotifiubyfy-users.herokuapp.com/users/user_by_id/" + artistId
+            val getRequest = JsonArrayRequest(
+                Request.Method.GET,
+                url, null,
+                Response.Listener { response ->
+                    val jsonArtist = JSONObject(response.get(0).toString())
+                    val username = jsonArtist.getString("username")
+                    val id = jsonArtist.getString("id").toInt()
+                    chatList.addArtistWithIdToPositionInList(Artist(id, username, image_link),
+                                                            position, false)
+                },
+                { error -> val intent = Intent(context, PopUpWindow::class.java).apply {
+//                    val error = errorResponse//.networkResponse.data.decodeToString() //.split('"')[3]
+                    putExtra("popuptext", error.toString())
+                }
+                    context.startActivity(intent)})
+            MyRequestQueue.getInstance(context).addToRequestQueue(getRequest)
+        }
+
         fun getChatsOfArtistWithID(context: Context, artist_id: Int,
                                    callBack: VolleyCallBack<ChatBundle>) {
-            val chatList = ArrayList<ChatBundle>()
-            chatList.add(ChatBundle(Artist(1, "The Beatles", image_link), false))
+            val url = "https://spotifiubyfy-messages.herokuapp.com/messages/all_conversations/" +
+                    artist_id + "?skip=0&limit=100"
+            val getRequest = JsonArrayRequest(
+                Request.Method.GET,
+                url, null,
+                Response.Listener { response ->
+                    this.searchRespectiveArtists(context, response, callBack)
+                },
+                { error -> val intent = Intent(context, PopUpWindow::class.java).apply {
+//                    val error = errorResponse//.networkResponse.data.decodeToString() //.split('"')[3]
+                    putExtra("popuptext", error.toString())
+                }
+                    context.startActivity(intent)})
+            MyRequestQueue.getInstance(context).addToRequestQueue(getRequest)
+            /*chatList.add(ChatBundle(Artist(1, "The Beatles", image_link), false))
             chatList.add(ChatBundle(Artist(1, "The Rollings", image_link), true))
             chatList.add(ChatBundle(Artist(1, "Adele", image_link), true))
             chatList.add(ChatBundle(Artist(1, "Perrito", image_link), true))
-            chatList.add(ChatBundle(Artist(1, "El Bicho", image_link), false))
-            callBack.updateData(chatList)
+            chatList.add(ChatBundle(Artist(1, "El Bicho", image_link), false))*/
         }
 
         fun getConversationBetween(context: Context, requesterId: Int, otherId: Int,
