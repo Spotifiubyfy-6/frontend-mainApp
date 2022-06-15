1bab3d1221879b569f041171dfdd14d0adc0fc8d
andrezszambrano <andreskilla2000@gmail.com>
2022-06-15 16:03:28 -0300

    Refactor: ArtistChatViewHolderr is now abstract and ArtistChatSeenViewHolder extends from it. Works as expected

--- app/src/main/java/com/example/spotifiubyfy01/Messages/adapter/ArtistChatViewHolder.kt
+++ app/src/main/java/com/example/spotifiubyfy01/Messages/adapter/ArtistChatViewHolder.kt
@@ -24,30 +24,37 @@ fun createRespectiveArtistChatHolder(parent: ViewGroup, viewType: Int) : ArtistC
                     .inflate(R.layout.layout_artist_chat_list_item, parent, false)
                 val artistName = view.findViewById<TextView>(R.id.artist_name)
                 artistName.setTypeface(null, Typeface.BOLD)
-                ArtistChatViewHolder(view)
+                ArtistChatSeenViewHolder(view)
             }
             ChatBundleEnum.CHAT_SEEN -> {
                 val view = LayoutInflater.from(parent.context)
                     .inflate(R.layout.layout_artist_chat_list_item, parent, false)
-                ArtistChatViewHolder(view)
+                ArtistChatSeenViewHolder(view)
             }
         }
     return holder
 }
 
+abstract class ArtistChatViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-class ArtistChatViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    abstract fun render(chatBundle: ChatBundle, position: Int,
+                        onClickListener: (ArtistChatViewHolder, ChatBundle, Int) -> Unit)
+
+    abstract fun changeToSeen()
+}
+
+class ArtistChatSeenViewHolder(view: View) : ArtistChatViewHolder(view) {
     private val artistName: TextView = view.findViewById(R.id.artist_name)
     private val image: ImageView = view.findViewById(R.id.artist_image)
 
-    fun render(chatBundle: ChatBundle, position: Int,
-               onClickListener: (ArtistChatViewHolder, ChatBundle, Int) -> Unit) {
+    override fun render(chatBundle: ChatBundle, position: Int,
+                        onClickListener: (ArtistChatViewHolder, ChatBundle, Int) -> Unit) {
         artistName.text = chatBundle.artist.username
         Glide.with(image.context).load(chatBundle.artist.image).into(image)
         itemView.setOnClickListener { onClickListener(this, chatBundle, position) }
     }
 
-    fun changeToSeen() {
+    override fun changeToSeen() {
         artistName.setTypeface(null, Typeface.NORMAL)
     }
 }
