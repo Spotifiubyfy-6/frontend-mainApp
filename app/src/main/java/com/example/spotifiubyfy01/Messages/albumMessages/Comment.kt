c028e85cf01bacb1450b1a57dff38c11d966c411
andrezszambrano <andreskilla2000@gmail.com>
2022-06-27 15:44:41 -0300

    Comment implements a method to get the time difference between time of comment and now

--- app/src/main/java/com/example/spotifiubyfy01/Messages/albumMessages/Comment.kt
+++ app/src/main/java/com/example/spotifiubyfy01/Messages/albumMessages/Comment.kt
@@ -1,12 +1,19 @@
 package com.example.spotifiubyfy01.Messages.albumMessages
 
+import android.text.format.DateUtils.getRelativeTimeSpanString
 import java.io.Serializable
 import com.example.spotifiubyfy01.search.Artist
-import java.time.LocalDateTime
+import java.time.*
 
 data class Comment(
     val artist: Artist,
     val comment: String,
     val time: LocalDateTime
 ) : Serializable {
+
+    fun getTimeAgo(): String {
+        val dif = LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli() -
+                time.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli()
+        return toDuration(dif)
+    }
 }
