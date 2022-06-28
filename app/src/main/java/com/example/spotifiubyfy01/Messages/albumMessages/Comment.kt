6593c799a0bd521a5bcac09054160da21d61a172
andrezszambrano <andreskilla2000@gmail.com>
2022-06-28 15:29:18 -0300

    Comment makes the right transformation

--- app/src/main/java/com/example/spotifiubyfy01/Messages/albumMessages/Comment.kt
+++ app/src/main/java/com/example/spotifiubyfy01/Messages/albumMessages/Comment.kt
@@ -1,9 +1,11 @@
 package com.example.spotifiubyfy01.Messages.albumMessages
 
 import android.text.format.DateUtils.getRelativeTimeSpanString
+import android.util.Log
 import java.io.Serializable
 import com.example.spotifiubyfy01.search.Artist
 import java.time.*
+import java.time.ZoneOffset.UTC
 
 data class Comment(
     var artist: Artist,
@@ -13,8 +15,7 @@ data class Comment(
 ) : Serializable {
 
     fun getTimeAgo(): String {
-        val dif = LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli() -
-                time.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli()
+        val dif = System.currentTimeMillis()- time.atZone(UTC).toInstant().toEpochMilli()
         return toDuration(dif)
     }
 }
