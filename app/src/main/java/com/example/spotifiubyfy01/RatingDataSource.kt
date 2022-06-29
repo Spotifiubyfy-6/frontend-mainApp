f223e06e94864198451c26704de610d53cd3fed3
andrezszambrano <andreskilla2000@gmail.com>
2022-06-29 15:32:02 -0300

    PostReview method is implemented. It hits the api, rating the given album

--- app/src/main/java/com/example/spotifiubyfy01/RatingDataSource.kt
+++ app/src/main/java/com/example/spotifiubyfy01/RatingDataSource.kt
@@ -1,8 +1,12 @@
 package com.example.spotifiubyfy01
 
 import android.content.Context
+import android.util.Log
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.StringRequest
+import com.example.spotifiubyfy01.Messages.MessagesDataSource
+import com.example.spotifiubyfy01.Messages.albumMessages.Comment
 import org.json.JSONObject
 
 class RatingDataSource {
@@ -36,6 +40,28 @@ class RatingDataSource {
 
                 })
             MyRequestQueue.getInstance(context).addToRequestQueue(getRequest)
+        }
+
+        fun postReview(context: Context, rating: Int, albumId: Int, userId: Int) {
+            val url = "http://spotifiubyfy-music.herokuapp.com/reviews"
+            val jsonRequest: StringRequest = object : StringRequest(
+                Method.POST, url, {},
+                { errorResponse ->
+                    Log.d("TAG", errorResponse.toString())
+                }) {
+                override fun getBodyContentType(): String {
+                    return "application/json"
+                }
+
+                override fun getBody(): ByteArray {
+                    val params2 = HashMap<String, String>()
+                    params2["user_id"] = userId.toString()
+                    params2["album_id"] = albumId.toString()
+                    params2["quantitative_review"] = rating.toString()
+                    return JSONObject(params2 as Map<String, String>).toString().toByteArray()
+                }
+            }
+            MyRequestQueue.getInstance(context).addToRequestQueue(jsonRequest)
         }
     }
 }
