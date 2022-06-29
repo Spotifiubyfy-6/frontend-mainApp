4b9ecdb735cd9ad61e6ee6a50291f85b1798571d
andrezszambrano <andreskilla2000@gmail.com>
2022-06-29 14:47:25 -0300

    setAlbumAverage callback function is created. Once the number of reviews, the total review sum and the user review is obtained, it sets the views accordingly

--- app/src/main/java/com/example/spotifiubyfy01/StarRatingHandler.kt
+++ app/src/main/java/com/example/spotifiubyfy01/StarRatingHandler.kt
@@ -1,15 +1,38 @@
 package com.example.spotifiubyfy01
 
+import android.content.Context
 import android.util.Log
 import android.widget.RatingBar
 import android.widget.TextView
 
-class StarRatingHandler(val starBar: RatingBar, val findViewById: TextView, val userId: Int) {
-
+class StarRatingHandler(private val starBar: RatingBar, val averageRating: TextView,
+                        val albumId: Int, userId: Int, val context: Context) {
+    private var userHasRated = false
+    init {
+        RatingDataSource.getReviewsAverage(context, albumId, userId, this::setAlbumAverage)
+    }
     fun watchNUpdateRating() {
-        starBar.rating = 4F
-        starBar.setOnRatingBarChangeListener { ratingBar, rating, fromUser ->
+
+        starBar.setOnRatingBarChangeListener { _, rating, _ ->
             Log.d("TAG", rating.toString())
         }
+    }
+
+    private fun setAlbumAverage(reviewSum: Int, numberOfReviews: Int, userReview: Int) {
+        if (userReview >= 0) {
+            starBar.rating = userReview.toFloat()
+            userHasRated = true
+        } else {
+            starBar.rating = 0F
+        }
+        val averageString = StringBuffer()
+        if (numberOfReviews == 0)
+            averageString.append("NF")
+        else {
+            val average = reviewSum / numberOfReviews
+            averageString.append(average.toString())
+        }
+        averageString.append("/5")
+        averageRating.text = averageString
     }
 }
