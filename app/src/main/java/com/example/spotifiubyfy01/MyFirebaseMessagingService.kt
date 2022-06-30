bebeb40807667fb8e7ec816b2ab14a3f3c9905c9
andrezszambrano <andreskilla2000@gmail.com>
2022-06-30 13:41:26 -0300

    Notification printed when app is opened

--- app/src/main/java/com/example/spotifiubyfy01/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/spotifiubyfy01/MyFirebaseMessagingService.kt
@@ -1,8 +1,12 @@
 package com.example.spotifiubyfy01
 
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService: FirebaseMessagingService() {
 
-
+    override fun onMessageReceived(message: RemoteMessage) {
+        Log.d("TAG", "notification received!")
+    }
 }
