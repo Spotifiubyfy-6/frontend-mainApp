0180b50871f5b654391ad2543589a3324991f761
andrezszambrano <andreskilla2000@gmail.com>
2022-06-30 13:35:09 -0300

    Device Token is printed to screen after logging in

--- app/src/main/java/com/example/spotifiubyfy01/LogInPage.kt
+++ app/src/main/java/com/example/spotifiubyfy01/LogInPage.kt
@@ -3,10 +3,12 @@ package com.example.spotifiubyfy01
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.toolbox.StringRequest
+import com.google.firebase.messaging.FirebaseMessaging
 
 
 class LogInPage : AppCompatActivity() {
@@ -26,6 +28,7 @@ class LogInPage : AppCompatActivity() {
 
             val jsonRequest: StringRequest = object : StringRequest(
                 Method.POST, url, { response ->
+                    setNotification()
                     val intent = Intent(this, MainPage::class.java).apply {
                     val sharedPref = getSharedPreferences(getString(R.string.token_key), Context.MODE_PRIVATE)
                     with (sharedPref.edit()) {
@@ -45,6 +48,13 @@ class LogInPage : AppCompatActivity() {
             }
             MyRequestQueue.getInstance(this).addToRequestQueue(jsonRequest)
         }
-
     }
+
+    private fun setNotification() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener {
+            val token = it.result
+            Log.d("TAG", token)
+        }
+    }
+
 }
