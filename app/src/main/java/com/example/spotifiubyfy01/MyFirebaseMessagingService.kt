1e3d6624533a6bbabbd6a2726b6fd92794251d01
andrezszambrano <andreskilla2000@gmail.com>
2022-07-01 14:50:40 -0300

    Notifications data is displayed and its shown on top of the screen

--- app/src/main/java/com/example/spotifiubyfy01/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/spotifiubyfy01/MyFirebaseMessagingService.kt
@@ -1,15 +1,32 @@
 package com.example.spotifiubyfy01
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.util.Log
+import androidx.core.app.NotificationChannelCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.getSystemService
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService: FirebaseMessagingService() {
 
-    override fun onMessageReceived(message: RemoteMessage) {
-        if (message.data.isNotEmpty()){
-            Log.d("TAG", "Message data payload: " + message.data);
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        if (remoteMessage.data.isNotEmpty()){
+            val title = "Message from " + (remoteMessage.data["name"] as String)
+            val message = remoteMessage.data["message"] as String
+            val CHANNEL_ID = "HEADS_UP_NOTIFICATION"
+            val channel = NotificationChannel(CHANNEL_ID, "Heads Up Notification",
+                                                 NotificationManager.IMPORTANCE_HIGH)
+            (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager).createNotificationChannel(channel)
+            val notification = Notification.Builder(this, CHANNEL_ID)
+                .setContentTitle(title).setContentText(message).setAutoCancel(true)
+                .setSmallIcon(R.drawable.ic_launcher_background).setAutoCancel(true)
+            NotificationManagerCompat.from(this).notify(1, notification.build())
+            Log.d("TAG", "Message data payload: " + remoteMessage.data);
         }
-        Log.d("TAG", "notification received!")
+        super.onMessageReceived(remoteMessage)
     }
 }
