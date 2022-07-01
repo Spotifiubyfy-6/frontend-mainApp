a6723d861a4a4fa5d5d21541e3382a42f59a6fac
andrezszambrano <andreskilla2000@gmail.com>
2022-07-01 20:39:01 -0300

    After a login, the notification token is sent

--- app/src/main/java/com/example/spotifiubyfy01/LogInPage.kt
+++ app/src/main/java/com/example/spotifiubyfy01/LogInPage.kt
@@ -7,8 +7,12 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.AuthFailureError
+import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.google.firebase.messaging.FirebaseMessaging
+import org.json.JSONObject
+import kotlin.reflect.KFunction1
 
 
 class LogInPage : AppCompatActivity() {
@@ -28,14 +32,16 @@ class LogInPage : AppCompatActivity() {
 
             val jsonRequest: StringRequest = object : StringRequest(
                 Method.POST, url, { response ->
-                    setNotification()
                     val intent = Intent(this, MainPage::class.java).apply {
                     val sharedPref = getSharedPreferences(getString(R.string.token_key), Context.MODE_PRIVATE)
                     with (sharedPref.edit()) {
                         putString(getString(R.string.token_key), response.split('"')[3])
                         apply()
                     }
+
                 }
+                    Log.d("TAG", "logging in")
+                    getId(this::setNotification)
                     startActivity(intent)},
                 { errorResponse -> val intent = Intent(this, PopUpWindow::class.java).apply {
 //                    val error = errorResponse.networkResponse.data.decodeToString().split('"')[3]
@@ -50,9 +56,51 @@ class LogInPage : AppCompatActivity() {
         }
     }
 
-    private fun setNotification() {
+    private fun getId(setNotification: KFunction1<String, Unit>) {
+        val url = "https://spotifiubyfy-users.herokuapp.com/users/me"
+        val getRequest: StringRequest = object : StringRequest(
+            Method.GET, url,
+            Response.Listener { response -> // response
+                val responseJson = JSONObject(response)
+                setNotification(responseJson.getString("id").toString())
+            },
+            { errorResponse ->
+            }
+        ) {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String> {
+                val params: MutableMap<String, String> = java.util.HashMap()
+                params["Authorization"] = "Bearer " + getSharedPreferences(getString(R.string.token_key), Context.MODE_PRIVATE).getString(getString(R.string.token_key), null)
+                return params
+            }
+        }
+        MyRequestQueue.getInstance(this).addToRequestQueue(getRequest)
+    }
+
+    private fun setNotification(id: String) {
+        Log.d("TAG", "User with id: " + id)
         FirebaseMessaging.getInstance().token.addOnCompleteListener {
             val token = it.result
+            val url = "https://spotifiubyfy-messages.herokuapp.com/messages/set_notification_token"
+            val jsonRequest: StringRequest = object : StringRequest(
+                Method.POST, url, { response ->
+                    Log.d("TAG", "Token set")
+                                  },
+                { errorResponse ->
+                    Log.d("TAG", errorResponse.toString())
+                }) {
+                override fun getBodyContentType(): String {
+                    return "application/json"
+                }
+
+                override fun getBody(): ByteArray {
+                    val params2 = HashMap<String, String>()
+                    params2["user_id"] = id
+                    params2["token"] = token
+                    return JSONObject(params2 as Map<String, String>).toString().toByteArray()
+                }
+            }
+            MyRequestQueue.getInstance(this).addToRequestQueue(jsonRequest)
             Log.d("TAG", token)
         }
     }
