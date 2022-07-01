577173770423e701f9ab226f309036f754d551b8
andrezszambrano <andreskilla2000@gmail.com>
2022-07-01 13:16:32 -0300

    onMessageReceived prints data

--- app/src/main/java/com/example/spotifiubyfy01/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/spotifiubyfy01/MyFirebaseMessagingService.kt
@@ -7,6 +7,9 @@ import com.google.firebase.messaging.RemoteMessage
 class MyFirebaseMessagingService: FirebaseMessagingService() {
 
     override fun onMessageReceived(message: RemoteMessage) {
+        if (message.data.isNotEmpty()){
+            Log.d("TAG", "Message data payload: " + message.data);
+        }
         Log.d("TAG", "notification received!")
     }
 }
