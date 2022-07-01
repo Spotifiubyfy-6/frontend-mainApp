4c45058b7c390a892799edd2e06d55683dbfd633
andrezszambrano <andreskilla2000@gmail.com>
2022-07-01 18:49:23 -0300

    CurrentActivityReceiver gets data and displays notification. If notification is clicked according activity ChatPage is navigated into

--- app/src/main/java/com/example/spotifiubyfy01/CurrentActivityReceiver.kt
+++ app/src/main/java/com/example/spotifiubyfy01/CurrentActivityReceiver.kt
@@ -1,18 +1,21 @@
 package com.example.spotifiubyfy01
 
 import android.app.Activity
-import android.app.NotificationChannel
+import android.app.Notification
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_IMMUTABLE
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat.getSystemService
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
-import android.app.Notification
-import android.app.NotificationManager
 import androidx.core.app.NotificationManagerCompat
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.example.spotifiubyfy01.Messages.ChatPage
+import com.example.spotifiubyfy01.search.Artist
+
 
 const val CURRENT_ACTIVITY_ACTION = "current.activity.action";
 val CURRENT_ACTIVITY_RECEIVER_FILTER = IntentFilter(CURRENT_ACTIVITY_ACTION);
@@ -21,10 +24,20 @@ val CURRENT_ACTIVITY_RECEIVER_FILTER = IntentFilter(CURRENT_ACTIVITY_ACTION);
 
 class CurrentActivityReceiver(private val receivingActivity: Activity): BroadcastReceiver() {
 
-    override fun onReceive(sender: Context, intent: Intent) {
+    override fun onReceive(context: Context, intent: Intent) {
         Log.d("TAG", "class " + getClassName(receivingActivity) + " is running!")
-        val title = intent.extras!!.get("title") as String
+        val idSender = (intent.extras!!.get("idSender") as String).toInt()
         val message = intent.extras!!.get("message") as String
+        val artistName = intent.extras!!.get("name") as String
+        val image = intent.extras!!.get("image") as String
+        val date = intent.extras!!.get("date") as String
+        val artist = Artist(idSender, artistName, image)
+        val myIntent = Intent(context, ChatPage::class.java)
+        myIntent.putExtra("other", artist)
+        myIntent.putExtra("requester_id",
+            (receivingActivity.application as Spotifiubify).getProfileData("id")!!.toInt())
+        val pendingIntent = PendingIntent.getActivity(context,0, myIntent,
+                                                    FLAG_UPDATE_CURRENT or FLAG_IMMUTABLE)
 
        /* val CHANNEL_ID = "HEADS_UP_NOTIFICATION"
         val channel = NotificationChannel(CHANNEL_ID, "Heads Up Notification",
@@ -33,8 +46,9 @@ class CurrentActivityReceiver(private val receivingActivity: Activity): Broadcas
         */
         val CHANNEL_ID = "HEADS_UP_NOTIFICATION"
         val notification = Notification.Builder(receivingActivity, CHANNEL_ID)
-            .setContentTitle(title).setContentText(message).setAutoCancel(true)
-            .setSmallIcon(R.drawable.ic_launcher_background).setAutoCancel(true)
+            .setContentTitle("New message from: " + artistName).setContentText(message)
+            .setAutoCancel(true).setSmallIcon(R.drawable.ic_launcher_background).setAutoCancel(true)
+            .setContentIntent(pendingIntent)
         NotificationManagerCompat.from(receivingActivity).notify(1, notification.build())
     }
 
